b3fcfedc5acb237bd401e6ab05c6fad6aa45a426
bingxiangch <109845864+bingxiangch@users.noreply.github.com>
2023-11-01 21:23:55 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
 from router.api import router  # Import your API router
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
 app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+# Define the list of allowed origins (replace "*" with your frontend's actual URL)
+origins = ["*"]
+
+# Add the CORS middleware to your FastAPI app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
