fa4521f4d28d68a07382666f0647e9c33262a4b4
bingxiang.chen <bingxiang.chen@tuni.fi>
2023-12-05 21:06:35 +0200

    optimize backend

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -1,3 +1,3 @@
-from .speech_to_text import LocalSpeechRecognition
+from .speech_to_text import SpeechToTextService
 from .text_to_speech import TextToSpeechService
 from .text_generation import TextGenerationService
